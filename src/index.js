9084022f5cb85693758f58dd7f134fed56f1f64e
marianappinheiro <maripinheiroazul@gmail.com>
2024-11-08 17:56:23 -0300

    Desafio do Herói executado com sucesso

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-let nome = "felipe";
-console.log(nome);
+const readline = require("readline")
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+rl.question("Digite seu nome: ", (nomeHeroi) => {
+    rl.question("Digite seu nível de XP: ", (nivelXp) => {
+        
+        nivelXp = parseInt(nivelXp, 10)
+
+        if (nivelXp < 1000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Ferro")
+        } else if (nivelXp > 1000 && nivelXp <= 2000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Bronze")
+        } else if (nivelXp > 2000 && nivelXp <= 5000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Prata")
+        } else if (nivelXp > 5000 && nivelXp <= 7000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Ouro")
+        } else if (nivelXp > 7000 && nivelXp <= 8000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Platina")
+        } else if (nivelXp > 8000 && nivelXp <= 9000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Ascendente")
+        } else if (nivelXp > 9000 && nivelXp <= 10000) {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Imortal")
+        } else {
+            console.log("O herói " + nomeHeroi + " está no nível " + "Radiante")
+        } 
+
+        let vidaExtra = 0
+
+        for (i = 0; i <= nivelXp; i++) {
+            vidaExtra += (0.01) 
+        } 
+
+console.log("O heroi " + nomeHeroi + " possui " + Math.round(vidaExtra) + " vidas") 
+
+        rl.close()
+    })
+})
